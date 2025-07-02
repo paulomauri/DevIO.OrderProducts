5191b71ad9b40196262b890763cb083a2c6455b6
Paulo Mauri <paulo.mauri@gmail.com>
2025-07-02 15:34:55 -0300

    Mensageria com Kafka

--- DevIO.OrderProducts.Application/Handlers/Produto/DeleteProdutoHandler.cs
+++ DevIO.OrderProducts.Application/Handlers/Produto/DeleteProdutoHandler.cs
@@ -1,5 +1,7 @@
 ﻿using DevIO.OrderProducts.Application.Commands.Produto;
+using DevIO.OrderProducts.Application.Events.Produto;
 using DevIO.OrderProducts.Application.Interfaces;
+using DevIO.OrderProducts.Application.Interfaces.Messaging;
 using DevIO.OrderProducts.Domain.Interfaces;
 using MediatR;
 
@@ -10,22 +12,34 @@ public class DeleteProdutoHandler
     private readonly IProdutoRepository _produtoRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IRedisCacheService _cache;
+    private readonly IKafkaProducerService _kafka;
     private const string CacheKey = "produto";
 
-    public DeleteProdutoHandler(IProdutoRepository produtoRepository, IUnitOfWork unitOfWork, IRedisCacheService cache)
+    public DeleteProdutoHandler(IProdutoRepository produtoRepository, IUnitOfWork unitOfWork, IRedisCacheService cache, IKafkaProducerService kafka)
     {
         _produtoRepository = produtoRepository;
         _unitOfWork = unitOfWork;
         _cache = cache;
+        _kafka = kafka;
     }
 
     public async Task<Unit> Handle(DeleteProdutoCommand request, CancellationToken cancellationToken)
     {
         var produto = await _produtoRepository.ObterPorIdAsync(request.Id);
         if (produto == null) throw new KeyNotFoundException("Produto não encontrado.");
+        
         await _produtoRepository.RemoverAsync(produto);
+        
         await _unitOfWork.CommitAsync(cancellationToken); 
+        
         await _cache.RemoveAsync(CacheKey);
+
+        // Publicar evento
+        await _kafka.ProduceAsync("produto-deletado", new ProdutoDeletadoEvent
+        {
+            ProdutoId = produto.Id,
+            DataExclusao = DateTime.UtcNow
+        });
 
         return Unit.Value;
     }
--- DevIO.OrderProducts.Worker/Program.cs
+++ DevIO.OrderProducts.Worker/Program.cs
@@ -5,6 +5,7 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<PedidoCriadoConsumerService>();
 builder.Services.AddHostedService<ProdutoAtualizadoConsumerService>();
 builder.Services.AddHostedService<PedidoAtualizadoConsumerService>();
+builder.Services.AddHostedService<ProdutoDeletadoConsumerService>();
 
 var host = builder.Build();
 
